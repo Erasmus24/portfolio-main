49b521ab81af631406bb89aa3f042ba496773a68
Erasmus Hadebe <erasmus.hadebe@detegoglobal.com>
2024-10-09 10:07:47 +0200

    The view button is fixed and working correctly

--- src/sections/portfolio/Project.jsx
+++ src/sections/portfolio/Project.jsx
@@ -1,5 +1,4 @@
-
-//import Card from '../../components/Card';
+// import Card from '../../components/Card'; // Uncomment if you plan to use Card
 
 const Project = ({ project }) => {
   return (
@@ -7,17 +6,18 @@ const Project = ({ project }) => {
       <div className="flip-card-inner">
         {/* Front of the card */}
         <div className="flip-card-front">
-             <div className="portfolio__project-image">
-              <h2 className='front-title'>{project.frontTitle}</h2>
-              <img src={project.image}  alt={`Portfolio project: ${project.title}`} loading="lazy" />
-            </div>
+          <div className="portfolio__project-image">
+            <h2 className='front-title'>{project.frontTitle}</h2>
+            <img src={project.image} alt={`Portfolio project: ${project.title}`} loading="lazy" />
+          </div>
         </div>
         {/* Back of the card */}
         <div className="flip-card-back">
-          <h3>{project.title}</h3>
-          <br></br>
+          <div>
+            <h3>{project.title}</h3>
             <p>{project.desc}</p>
-            <a 
+          </div>
+          <a 
             href={project.demo} // Ensure this points to the correct demo URL
             className="btn view-button"
             target="_blank"
