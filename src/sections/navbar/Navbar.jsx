6175933fa4a8043ed6f6c78f86ef9065efae605c
Erasmus Hadebe <erasmus.hadebe@detegoglobal.com>
2024-11-19 09:23:52 +0200

    added animation to the change theme icon

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,27 +1,48 @@
-import Logo from '../../assets/Erasmus_Hadebe.jpg'
-import data from './data'
-import {IoIosColorPalette} from 'react-icons/io'
-import { useModalContext } from '../../context/modal-context'
-import './navbar.css'
+import { useEffect, useState } from 'react';
+import Logo from '../../assets/Erasmus_Hadebe.jpg';
+import data from './data';
+import { IoIosColorPalette } from 'react-icons/io';
+import { useModalContext } from '../../context/modal-context';
+import './navbar.css';
 
 const Navbar = () => {
-  const {showModalHandler} = useModalContext();
+  const { showModalHandler } = useModalContext();
+  const [isShaking, setIsShaking] = useState(false);
+
+  useEffect(() => {
+    // Trigger the shake animation when the component mounts for the first time
+    setIsShaking(true);
+    const timer = setTimeout(() => {
+      setIsShaking(false); // Stop the animation after 4 seconds
+    }, 4000);
+
+    return () => clearTimeout(timer); // Cleanup timer on component unmount
+  }, []);
 
   return (
     <nav>
       <div className="container nav__container">
-        <a href="index.html" className='nav__logo'>
+        <a href="index.html" className="nav__logo">
           <img src={Logo} alt="Logo" />
         </a>
-        <ul className='nav__menu'>
-          {
-            data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
-          }
+        <ul className="nav__menu">
+          {data.map(item => (
+            <li key={item.id}>
+              <a href={item.link}>{item.title}</a>
+            </li>
+          ))}
         </ul>
-        <button id='theme__icon' onClick={showModalHandler} title="Change Theme"><IoIosColorPalette/></button>
+        <button
+          id="theme__icon"
+          onClick={showModalHandler}
+          title="Change Theme"
+          className={isShaking ? 'shake' : ''}
+        >
+          <IoIosColorPalette />
+        </button>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
